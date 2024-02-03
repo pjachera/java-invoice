0d4c98a580341f505a46e29a2cb17f34be7bcc9e
jachera <pjachera@gmail.com>
2024-02-03 12:14:19 +0100

    Not working yet :)

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,30 +1,43 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-    private Collection<Product> products;
+
+
+    private Map<Product, Integer> products = new HashMap<>();
 
     public void addProduct(Product product) {
+       // this.products.add(product);
         // TODO: implement
     }
 
     public void addProduct(Product product, Integer quantity) {
+
+
         // TODO: implement
     }
 
-    public BigDecimal getSubtotal() {
-        return null;
+    public BigDecimal getNetPrice() {
+        BigDecimal netPrice = new BigDecimal(0);
+        for (Product product : products.keySet()) {
+            netPrice = netPrice.add(product.getPrice());
+        }
+
+        return netPrice;
     }
 
     public BigDecimal getTax() {
-        return null;
+        return BigDecimal.ZERO;
     }
 
-    public BigDecimal getTotal() {
-        return null;
+    public BigDecimal getGrossPrice() {
+        return BigDecimal.ZERO;
     }
 }
