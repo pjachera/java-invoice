174d767fb569fbabcca6fb06bb05352b076d7dc1
Piotr <pjachera@gmail.com>
2024-04-03 08:40:16 +0200

    Cwiczenie (1) Drukowanie

--- src/test/java/pl/edu/agh/mwo/invoice/InvoiceTest.java
+++ src/test/java/pl/edu/agh/mwo/invoice/InvoiceTest.java
@@ -113,37 +113,37 @@ public class InvoiceTest {
     }
 
     @Test
-    public void testInvoiceHaveANumber(){
+    public void testInvoiceHaveANumber() {
         Assert.assertNotNull(invoice.getNumber());
     }
 
     @Test
-    public void testInvoiceNumberIsGreaterThanZero(){
+    public void testInvoiceNumberIsGreaterThanZero() {
         Assert.assertTrue(invoice.getNumber() > 0);
     }
 
     @Test
-    public void testFirstInvoiceNumberIsLessThanSecond(){
+    public void testFirstInvoiceNumberIsLessThanSecond() {
         Assert.assertTrue(invoice.getNumber() < new Invoice().getNumber());
     }
 
     @Test
-    public void testPrintProductsFromInvoice(){
+    public void testPrintProductsFromInvoice() {
         invoice.addProduct(new DairyProduct("Chleb", new BigDecimal("4.5")));
         invoice.addProduct(new DairyProduct("Maslo", new BigDecimal("7")), 2);
         invoice.addProduct(new DairyProduct("Bulka", new BigDecimal("1")), 2);
         invoice.addProduct(new OtherProduct("Piwo", new BigDecimal("3")), 8);
 
-        Assert.assertEquals(invoice.print(), "Numer faktury:"+ invoice.getNumber() +"\n" +
-                                                   "Nazwa produktu:Chleb Liczba sztuk:1 Cena:4.5zł\n" +
-                                                   "Nazwa produktu:Maslo Liczba sztuk:2 Cena:7zł\n" +
-                                                   "Nazwa produktu:Bulka Liczba sztuk:2 Cena:1zł\n" +
-                                                   "Nazwa produktu:Piwo Liczba sztuk:8 Cena:3zł\n" +
-                                                   "Liczba pozycji: 4");
+        Assert.assertEquals(invoice.print(), "Numer faktury:" + invoice.getNumber() + "\n" +
+                "Nazwa produktu:Chleb Liczba sztuk:1 Cena:4.5zł\n" +
+                "Nazwa produktu:Maslo Liczba sztuk:2 Cena:7zł\n" +
+                "Nazwa produktu:Bulka Liczba sztuk:2 Cena:1zł\n" +
+                "Nazwa produktu:Piwo Liczba sztuk:8 Cena:3zł\n" +
+                "Liczba pozycji: 4");
     }
 
     @Test
-    public void testPrintEmptyInvoice(){
+    public void testPrintEmptyInvoice() {
         Assert.assertEquals(invoice.print(), "");
     }
 
